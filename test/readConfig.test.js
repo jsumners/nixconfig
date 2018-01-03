1d425c47bb8a07a1aeb0cba765b49ea8699e3ffa
James Sumners <james.sumners@gmail.com>
2018-01-03 18:18:01 -0500

    Fix #2

--- test/readConfig.test.js
+++ test/readConfig.test.js
@@ -4,44 +4,100 @@ const path = require('path')
 const test = require('tap').test
 const mockquire = require('mock-require')
 
-mockquire('../lib/parentPkg', () => {
-  return {
-    name: 'foo',
-    nixconfigPrefix: 'nixconfig_'
-  }
+test('loads object based config files', (t) => {
+  t.tearDown(() => {
+    mockquire.stopAll()
+  })
+
+  t.beforeEach((done) => {
+    mockquire.stopAll()
+
+    delete require.cache[require.resolve('../lib/readConfig')]
+    delete require.cache[require.resolve('../lib/lookupPaths')]
+    delete require.cache[require.resolve('../lib/nixconfig')]
+
+    mockquire('../lib/parentPkg', () => {
+      return {
+        name: 'foo',
+        nixconfigPrefix: 'nixconfig_'
+      }
+    })
+
+    mockquire('../lib/lookupPaths', [
+      path.join(__dirname, 'fixtures', 'etc'),
+      path.join(__dirname, 'fixtures', 'etc', 'foo')
+    ])
+
+    done()
+  })
+
+  t.test('loads files from /etc', (t) => {
+    t.plan(6)
+    const nixconfigProto = require('../lib/nixconfig')
+    const readConfig = require('../lib/readConfig')
+    const nixconfig = Object.create(nixconfigProto, {
+      config: {value: {}},
+      log: {value: require('abstract-logging')}
+    })
+    const config = readConfig(nixconfig)
+    t.ok(config.foo)
+    t.is(config.foo, 'foo')
+    t.ok(config.bar)
+    t.is(config.bar, 'bar')
+    t.ok(config.toOverride)
+    t.is(config.toOverride, 'b')
+  })
+
+  t.test('loads from environment', (t) => {
+    t.plan(2)
+    const nixconfigProto = require('../lib/nixconfig')
+    const readConfig = require('../lib/readConfig')
+    const nixconfig = Object.create(nixconfigProto, {
+      config: {value: {}},
+      log: {value: require('abstract-logging')}
+    })
+    process.env['nixconfig_foobar'] = 'foobar'
+    const config = readConfig(nixconfig)
+    t.ok(config.foobar)
+    t.is(config.foobar, 'foobar')
+    delete process.env['nixconfig_foobar']
+  })
+
+  t.end()
 })
 
-mockquire('../lib/lookupPaths', [
-  path.join(__dirname, 'fixtures', 'etc'),
-  path.join(__dirname, 'fixtures', 'etc', 'foo')
-])
+test('loads array based config files', (t) => {
+  t.plan(1)
 
-const nixconfigProto = require('../lib/nixconfig')
-const readConfig = require('../lib/readConfig')
+  delete require.cache[require.resolve('../lib/readConfig')]
+  delete require.cache[require.resolve('../lib/lookupPaths')]
+  delete require.cache[require.resolve('../lib/nixconfig')]
 
-test('loads files from /etc', (t) => {
-  t.plan(6)
+  mockquire('../lib/parentPkg', () => {
+    return {
+      name: 'foo',
+      nixconfigPrefix: 'nixconfig_'
+    }
+  })
+
+  mockquire('../lib/lookupPaths', [
+    path.join(__dirname, 'fixtures', 'bar')
+  ])
+
+  t.tearDown(() => {
+    mockquire.stopAll()
+  })
+
+  const nixconfigProto = require('../lib/nixconfig')
+  const readConfig = require('../lib/readConfig')
+
   const nixconfig = Object.create(nixconfigProto, {
     config: {value: {}},
     log: {value: require('abstract-logging')}
   })
   const config = readConfig(nixconfig)
-  t.ok(config.foo)
-  t.is(config.foo, 'foo')
-  t.ok(config.bar)
-  t.is(config.bar, 'bar')
-  t.ok(config.toOverride)
-  t.is(config.toOverride, 'b')
-})
-
-test('loads from environment', (t) => {
-  t.plan(2)
-  const nixconfig = Object.create(nixconfigProto, {
-    config: {value: {}},
-    log: {value: require('abstract-logging')}
+  t.strictDeepEqual(config, {
+    should: 'be in final config',
+    gets_overwritten: 'final'
   })
-  process.env['nixconfig_foobar'] = 'foobar'
-  const config = readConfig(nixconfig)
-  t.ok(config.foobar)
-  t.is(config.foobar, 'foobar')
 })
